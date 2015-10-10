afab2483900bf578b61df1732d78294b42d0b700
shawntan <shawn@wtf.sg>
2015-10-11 07:47:41 +0800

    STil underflowing

--- ctc1.py
+++ ctc1.py
@@ -10,10 +10,9 @@ eps = 1e-40
 def update_log_p(base,zeros,i,log_p_curr,log_p_prev):
     useful = T.min([(i + 1) * 2,log_p_prev.shape[0]])
     prev_useful = T.min([i * 2,log_p_prev.shape[0]])
-    comm_factor = log_p_prev[0]
     skip_idxs = T.arange((base.shape[0] - 2)//2) * 2 + 1
 
-    common_factor = log_p_prev[0]
+    common_factor = T.max(log_p_prev)
     p_prev   = T.exp(log_p_prev[:prev_useful] - common_factor) 
     p_prev   = T.set_subtensor(base[:prev_useful],p_prev)
     _p_prev  = T.inc_subtensor(p_prev[1:],p_prev[:-1])
@@ -35,7 +34,7 @@ def path_probs(predict, Y):
     def step(i,log_f_curr,log_b_curr,log_f_prev,log_b_prev):
         return update_log_p(base,zeros,i,log_f_curr,log_f_prev),\
                 update_log_p(base,zeros,i,log_b_curr,log_b_prev)
-    
+
     [log_f_probs,log_b_probs], _ = theano.scan(
             step,
             sequences=[T.arange(L.shape[0]),L,L[::-1, ::-1]],
@@ -43,15 +42,16 @@ def path_probs(predict, Y):
         )
 
     log_probs = log_f_probs + log_b_probs[::-1, ::-1] - L
-    
+
     return log_probs
 
 def cost(predict, Y):
     log_probs = path_probs(predict, Y)
-    comm_factor = T.min(log_probs)
+    common_factor = T.max(log_probs)
     idxs = 2 * T.arange(predict.shape[0])[::-1].dimshuffle(0,'x') + T.arange(Y.shape[0])
-    relevant_probs = (idxs < predict.shape[0] * Y.shape[0]) * (idxs >= predict.shape[0])
-    total_log_prob = T.log(T.sum(T.exp(log_probs - comm_factor) * relevant_probs)) + comm_factor
+    relevant_probs = (idxs < (predict.shape[0] * 2)) * (idxs >= Y.shape[0]-2)
+    total_log_prob = T.log(T.sum(T.exp(log_probs - common_factor) * relevant_probs)) + common_factor
+
     return -total_log_prob
 
 
--- ocr.py
+++ ocr.py
@@ -10,38 +10,17 @@ from theano_toolkit.parameters import Parameters
 import ctc1
 import ctc
 import font
+import lstm
 
+def build_model(P, input_size, hidden_size, output_size):
+    lstm_layer = lstm.build(P,"lstm",input_size,hidden_size)
+    P.W_output = np.zeros((hidden_size,output_size))
+    P.b_output = np.zeros((output_size,))
 
-def build_rnn(hidden_inputs, W_hidden_hidden, b_hidden,
-              initial_hidden):
-    def step(input_curr, hidden_prev):
-        hidden = T.tanh(
-            T.dot(hidden_prev, W_hidden_hidden) +
-            input_curr +
-            b_hidden
-        )
-        return hidden
-    hidden, _ = theano.scan(
-        step,
-        sequences=[hidden_inputs],
-        outputs_info=[initial_hidden]
-    )
-    return hidden
-
-
-def build_model(P, X, input_size, hidden_size, output_size):
-    P.W_input_hidden =  U.initial_weights(input_size, hidden_size)
-    P.W_hidden_hidden = U.initial_weights(hidden_size, hidden_size)
-    P.W_hidden_output = U.initial_weights(hidden_size, output_size)
-    P.b_hidden = U.initial_weights(hidden_size)
-    P.i_hidden = U.initial_weights(hidden_size)
-    P.b_output = U.initial_weights(output_size)
-    hidden = build_rnn(T.dot(X, P.W_input_hidden),
-                       P.W_hidden_hidden, P.b_hidden, P.i_hidden)
-
-    predict = T.nnet.softmax(T.dot(hidden, P.W_hidden_output) + P.b_output)
-
-    return X, predict
+    def model(X):
+        hidden = lstm_layer(X)[1]
+        return T.nnet.softmax(T.dot(hidden,P.W_output) + P.b_output)
+    return model
 
 
 def label_seq(string):
@@ -55,28 +34,49 @@ if __name__ == "__main__":
     P = Parameters()
     X = T.matrix('X')
     Y = T.ivector('Y')
-    X,predict = build_model(P,X,8,256,len(font.chars)+1)
+    predict = build_model(P,8,256,len(font.chars)+1)
 
-    cost = ctc1.cost(predict, Y)
+
+    probs = predict(X)
+    alpha = 0.1
+    smoothed_predict = (1-alpha) * probs + alpha * 1./(len(font.chars)+1)
+    cost = ctc1.cost(smoothed_predict, Y)
     params = P.values()
     gradients = T.grad(cost, wrt=params)
-    train = theano.function(
+    
+    gradient_acc = [ 
+            theano.shared(0 * p.get_value()) for p in params 
+        ]
+    counter = theano.shared(np.float32(0.))
+    acc = theano.function(
         inputs=[X, Y],
         outputs=cost,
-        updates = [ (p,p-1e-6 * g) for p,g in zip(params,gradients) ]
+        updates = [
+            (a,a + g) for a,g in zip(gradient_acc,gradients)
+        ] + [(counter,counter + np.float32(1.))]
     )
-    test = theano.function(
-            inputs=[X,Y],
-            outputs=predict[:,Y] / T.sum(predict[:,Y],axis=1).dimshuffle(0,'x')
+    update = theano.function(
+            inputs=[],outputs=[],
+            updates = updates.rmsprop(
+                params,[ g / counter for g in gradient_acc ],
+                learning_rate=1e-5
+            ) + [ (counter,np.float32(0.))]
         )
-    training_examples = "the quick brown fox jumps over the lazy dog".split()
+
+    test = theano.function(
+         inputs=[X,Y],
+         outputs=probs[:,Y] / T.sum(probs[:,Y],axis=1).dimshuffle(0,'x')
+        )
+
+    training_examples = [ word.strip() for word in open('dictionary.txt') ]
     import random
-    for _ in xrange(1000):
+    for _ in xrange(1500):
         random.shuffle(training_examples)
-        for string in training_examples:
-            print train(font.imagify(string),label_seq(string))
-        hinton.plot(test(font.imagify("the"),label_seq("the")).T,max_arr=1)
-        hinton.plot(font.imagify("the").T[::-1].astype('float32'))
+        for string in training_examples[:100]:
+            print acc(font.imagify(string),label_seq(string))
+        update()
+        hinton.plot(test(font.imagify("test"),label_seq("test")).T)
+        hinton.plot(font.imagify("test").T[::-1].astype('float32'))
 
 
 
