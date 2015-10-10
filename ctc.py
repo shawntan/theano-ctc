7a90271ecd741fd4ef78cf11317bdfb70f202fe8
Shawn Tan <shawn@wtf.sg>
2015-10-10 19:48:34 +0800

    Differentiable CTC function
    Not working perfectly yet.

--- ctc.py
+++ ctc.py
@@ -1,65 +1,32 @@
 import theano
 import theano.tensor as T
-import numpy		 as np
+import numpy as np
 from theano_toolkit import utils as U
 from theano_toolkit import updates
 from theano.printing import Print
 
 eps = 1e-40
 
-def logplus(log_a,log_b):
-	# Returns log (a + b)
-	return log_a + T.log(1 + T.exp(log_b - log_a))
+def recurrence_relation(size):
+    big_I = T.eye(size+2)
+    return T.eye(size) + big_I[2:,1:-1] + big_I[2:,:-2] * T.cast(T.arange(size) % 2,'float32')
 
-def log_(a):
-	return T.log(T.clip(a,eps,1))
+def path_probs(predict, Y):
+    P = predict[:,Y]
+    rr = recurrence_relation(Y.shape[0])
+    def step(p_curr,p_prev):
+        result = p_curr * T.dot(p_prev,rr)
+        return result
+    probs,_ = theano.scan(
+            step,
+            sequences = [P],
+            outputs_info = [T.eye(Y.shape[0])[0]]
+        )
+    return probs
 
-def exp_(a):
-	return T.exp(T.clip(a,np.log(eps),30))
-
-def path_probs(predict,Y):
-	eye = T.eye(Y.shape[0])
-	first = eye[0]
-	mask0  = 1 - eye[0]
-	mask1  = 1 - eye[1]
-
-	alt_mask = T.cast(T.arange(Y.shape[0]) % 2,'float32')
-	skip_mask = mask0 * mask1 * alt_mask
-	prev_idx = T.arange(-1,Y.shape[0]-1)
-	prev_prev_idx = T.arange(-2,Y.shape[0]-2)
-
-	log_mask0     = log_(mask0)
-	log_skip_mask = log_(skip_mask)
-	log_first     = log_(first)
-
-	def step(log_p_curr,log_p_prev):
-		log_after_trans = logplus(
-				log_p_prev,
-				logplus(
-					log_mask0     + log_p_prev[prev_idx],
-					log_skip_mask + log_p_prev[prev_prev_idx]
-				)
-			)
-		log_p_next = log_p_curr + log_after_trans
-		return log_p_next
-
-	L = T.log(predict[:,Y])
-
-	log_f_probs,_ = theano.scan(step, sequences = [L],            outputs_info = [log_first])
-	log_b_probs,_ = theano.scan(step, sequences = [L[::-1,::-1]], outputs_info = [log_first])
-
-	log_probs = log_f_probs + log_b_probs[::-1,::-1]
-	return log_probs,prev_idx,prev_prev_idx
-
-def cost(predict,Y):
-	log_probs,prev_idx,prev_prev_idx  = path_probs(predict,Y)
-	max_log_prob = T.max(log_probs)
-
-	# logsumexp.
-	norm_probs = T.exp(log_probs - max_log_prob)
-	norm_probs = Print("Norm_probs")(norm_probs)
-	norm_total_log_prob = T.log(T.sum(norm_probs))
-	
-	log_total_prob = norm_total_log_prob + max_log_prob
-	return -log_total_prob
-
+def cost(predict, Y):
+    forward_probs  = path_probs(predict,Y)
+    backward_probs = path_probs(predict[::-1],Y[::-1])[::-1,::-1]
+    probs = forward_probs * backward_probs / predict[:,Y]
+    total_prob = T.sum(probs)
+    return -T.log(total_prob)
--- ocr.py
+++ ocr.py
@@ -1,67 +1,82 @@
+# coding=utf-8
 import theano
 import theano.tensor as T
-import numpy		 as np
+import numpy as np
 from theano_toolkit import utils as U
+from theano_toolkit import hinton
 from theano_toolkit import updates
 from theano_toolkit.parameters import Parameters
 
+import ctc1
 import ctc
 import font
 
-def build_rnn(hidden_inputs,W_hidden_hidden,b_hidden,
-		initial_hidden):
-	def step(input_curr,hidden_prev):
-		hidden = T.tanh(
-				T.dot(hidden_prev,W_hidden_hidden) +\
-				input_curr +\
-				b_hidden
-			)
-		return hidden
-	hidden,_ = theano.scan(
-		step,
-		sequences = [hidden_inputs],
-		outputs_info = [initial_hidden]
-	)
-	return hidden
+
+def build_rnn(hidden_inputs, W_hidden_hidden, b_hidden,
+              initial_hidden):
+    def step(input_curr, hidden_prev):
+        hidden = T.tanh(
+            T.dot(hidden_prev, W_hidden_hidden) +
+            input_curr +
+            b_hidden
+        )
+        return hidden
+    hidden, _ = theano.scan(
+        step,
+        sequences=[hidden_inputs],
+        outputs_info=[initial_hidden]
+    )
+    return hidden
 
 
+def build_model(P, X, input_size, hidden_size, output_size):
+    P.W_input_hidden =  U.initial_weights(input_size, hidden_size)
+    P.W_hidden_hidden = U.initial_weights(hidden_size, hidden_size)
+    P.W_hidden_output = U.initial_weights(hidden_size, output_size)
+    P.b_hidden = U.initial_weights(hidden_size)
+    P.i_hidden = U.initial_weights(hidden_size)
+    P.b_output = U.initial_weights(output_size)
+    hidden = build_rnn(T.dot(X, P.W_input_hidden),
+                       P.W_hidden_hidden, P.b_hidden, P.i_hidden)
 
-def build_model(P,X,input_size,hidden_size,output_size):
-	W_input_hidden  = U.create_shared(U.initial_weights(input_size,hidden_size))
-	W_hidden_hidden = U.create_shared(U.initial_weights(hidden_size,hidden_size))
-	W_hidden_output = U.create_shared(U.initial_weights(hidden_size,output_size))
-	b_hidden = U.create_shared(U.initial_weights(hidden_size))
-	i_hidden = U.create_shared(U.initial_weights(hidden_size))
-	b_output = U.create_shared(U.initial_weights(output_size))
-	hidden = build_rnn(T.dot(X,W_input_hidden),W_hidden_hidden,b_hidden,i_hidden)
+    predict = T.nnet.softmax(T.dot(hidden, P.W_hidden_output) + P.b_output)
 
-	predict = T.nnet.softmax(T.dot(hidden,W_hidden_output) + b_output)
+    return X, predict
 
-	return X,predict
 
 def label_seq(string):
-	idxs = font.indexify(string)
-	result = np.ones((len(idxs)*2 + 1,),dtype=np.int32) * -1
-	result[np.arange(len(idxs))*2+1] = idxs
-	print result
-	return result
+    idxs = font.indexify(string)
+    result = np.ones((len(idxs) * 2 + 1,), dtype=np.int32) * -1
+    result[np.arange(len(idxs)) * 2 + 1] = idxs
+    return result
 
 
 if __name__ == "__main__":
-	P = Parameters()
-	X = T.matrix('X')
-	Y = T.ivector('Y')
-	X,predict = build_model(P,X,8,256,len(font.chars)+1)
+    P = Parameters()
+    X = T.matrix('X')
+    Y = T.ivector('Y')
+    X,predict = build_model(P,X,8,256,len(font.chars)+1)
 
-	cost = ctc.cost(predict,Y)
-	params = P.values()
-	grad = T.grad(cost,wrt=params)
-	train = theano.function(
-			inputs=[X,Y],
-			outputs=cost,
-			updates=updates.adadelta(params,grad)
-		)
+    cost = ctc1.cost(predict, Y)
+    params = P.values()
+    gradients = T.grad(cost, wrt=params)
+    train = theano.function(
+        inputs=[X, Y],
+        outputs=cost,
+        updates = [ (p,p-1e-6 * g) for p,g in zip(params,gradients) ]
+    )
+    test = theano.function(
+            inputs=[X,Y],
+            outputs=predict[:,Y] / T.sum(predict[:,Y],axis=1).dimshuffle(0,'x')
+        )
+    training_examples = "the quick brown fox jumps over the lazy dog".split()
+    import random
+    for _ in xrange(1000):
+        random.shuffle(training_examples)
+        for string in training_examples:
+            print train(font.imagify(string),label_seq(string))
+        hinton.plot(test(font.imagify("the"),label_seq("the")).T,max_arr=1)
+        hinton.plot(font.imagify("the").T[::-1].astype('float32'))
 
-	string = "Shawn"
 
-	print train(font.imagify(string),label_seq(string))
+
