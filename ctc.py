7cab6310e4e049d82992cab804dafc1b587f0105
Shawn Tan <shawn@wtf.sg>
2016-06-07 13:47:02 +0800

    Label masking

--- ctc.py
+++ ctc.py
@@ -6,26 +6,33 @@ from theano_toolkit import updates
 from theano.printing import Print
 
 def recurrence(log_p_curr, t, log_p_prev):
-    idx = T.arange(log_p_prev.shape[0])
-    k = T.max(log_p_prev, axis=1)
-    # previous
-    p_prev = T.exp(log_p_prev - k)
-    p_prev = T.set_subtensor(p_prev[:,t:],0)
+    idx = T.arange(log_p_prev.shape[1])
+    mask = idx.dimshuffle('x',0) < t.dimshuffle(0,'x')
 
-    _result = p_prev
+    # normalise and bring back to p space
+    k = T.maximum(T.max(T.switch(mask,log_p_prev,-np.inf), axis=1), 0)
+    norm_p_prev = T.exp(log_p_prev - k)
+    norm_p_prev = T.switch(mask, norm_p_prev, 0) # set -inf to 0
+
+    # previous
+    _result = norm_p_prev
     # add shift of previous
-    _result = T.inc_subtensor(_result[1:],   p_prev[:-1])
+    _result = T.inc_subtensor(_result[:,1:],   norm_p_prev[:,:-1])
     # add skips of previous
-    _result = T.inc_subtensor(_result[3::2], p_prev[1:-2:2])
+    _result = T.inc_subtensor(_result[:,3::2], norm_p_prev[:,1:-2:2])
 
     # current
-    log_result = T.switch(t > 0, T.log(_result) + k, T.zeros_like(_result))
+    # log(p) should be 0 for first 2 terms
+    _result = T.switch((t > 0).dimshuffle(0,'x'),
+                log_p_curr + T.log(_result) + k,
+                log_p_curr
+            )
+
     t = t + 2
-    _recurrence = log_result + log_p_curr
-    recurrence = T.set_subtensor(_recurrence[t:], np.float32(0))
+    result = _result #T.set_subtensor(_result[:,t:], np.float32(0))
     non_inf = idx < t
 
-    return t, recurrence, non_inf
+    return t, result, non_inf
 
 
 if __name__ == "__main__":
@@ -45,14 +52,13 @@ if __name__ == "__main__":
                 outputs_info=[T.zeros((probs.shape[1],)), log_init_probs, None]
             )
         cost = log_forward[1]
-        grads = T.grad(-cost[-1,-1],wrt=[baseline.W_test])
-        f = theano.function(inputs=[X],outputs=[cost,grads[0],log_forward[2]])
+#        grads = T.grad(-cost[0,-1,-1],wrt=[baseline.W_test])
+        f = theano.function(inputs=[X],outputs=[cost,log_forward[2]])
         return f(inputs)
 
     inputs = np.random.randn(10,5).astype(np.float32)
     orig = base(inputs)
-    log_space, grads, non_inf = log_space(inputs)
-    print log_space - orig
+    log_space, non_inf = log_space(inputs)
+    print log_space[:,0,:] - orig
     print non_inf
-    print grads
 
